fd8df43fb49f5f6fd3e495da23ef8fa3c750985a
Michael Bucari-Tovo <mbucari1@gmail.com>
2025-10-29 13:40:21 -0600

    Use xplat webview control for Audible login
    - Use Avalonia-based webview control for Audible login with Chardonnay
    - Remove webview interfaces from IInteropFunctions
    - Remove Microsoft.Web.WebView2 package from WindowsConfigApp
    - Add Microsoft.Web.WebView2 to LibationWinForms
    - All login forms from chardonnay. The AudibleApi login with username/password doesn't work anymore. Need to use external browser login method.
    - Replace deprecated Avalonia.ReactiveUI with ReactiveUI.Avalonia

--- Source/LibationAvalonia/Dialogs/Login/AvaloniaLoginChoiceEager.cs
+++ Source/LibationAvalonia/Dialogs/Login/AvaloniaLoginChoiceEager.cs
@@ -1,5 +1,6 @@
 ﻿using AudibleApi;
 using AudibleUtilities;
+using Avalonia.Controls;
 using Avalonia.Threading;
 using LibationFileManager;
 using LibationUiBase.Forms;
@@ -11,14 +12,13 @@ namespace LibationAvalonia.Dialogs.Login
 {
 	public class AvaloniaLoginChoiceEager : ILoginChoiceEager
 	{
-		public ILoginCallback LoginCallback { get; }
+		public ILoginCallback LoginCallback { get; } = new AvaloniaLoginCallback();
 
 		private readonly Account _account;
 
 		public AvaloniaLoginChoiceEager(Account account)
 		{
 			_account = Dinah.Core.ArgumentValidator.EnsureNotNull(account, nameof(account));
-			LoginCallback = new AvaloniaLoginCallback(_account);
 		}
 
 		public async Task<ChoiceOut?> StartAsync(ChoiceIn choiceIn)
@@ -26,41 +26,84 @@ namespace LibationAvalonia.Dialogs.Login
 
 		private async Task<ChoiceOut?> StartAsyncInternal(ChoiceIn choiceIn)
 		{
-			if (Configuration.IsWindows && Environment.OSVersion.Version.Major >= 10)
+			try
 			{
-				try
-				{
-					var weblogin = new WebLoginDialog(_account.AccountId, choiceIn.LoginUrl);
-					if (await weblogin.ShowDialogAsync(App.MainWindow) is DialogResult.OK)
-						return ChoiceOut.External(weblogin.ResponseUrl);
-				}
-				catch (Exception ex)
-				{
-					Serilog.Log.Logger.Error(ex, $"Failed to run {nameof(WebLoginDialog)}");
-				}
+				if (await BrowserLoginAsync(choiceIn.LoginUrl) is ChoiceOut external)
+					return external;
+			}
+			catch (Exception ex)
+			{
+				Serilog.Log.Logger.Error(ex, $"Failed to use the {nameof(NativeWebDialog)}");
 			}
 
-			var dialog = new LoginChoiceEagerDialog(_account);
-
-			if (await dialog.ShowDialogAsync() is not DialogResult.OK ||
-				(dialog.LoginMethod is LoginMethod.Api && string.IsNullOrWhiteSpace(dialog.Password)))
-				return null;
-
-			switch (dialog.LoginMethod)
-			{
-				case LoginMethod.Api:
-					return ChoiceOut.WithApi(dialog.Account.AccountId, dialog.Password);
-				case LoginMethod.External:
-					{
-						var externalDialog = new LoginExternalDialog(_account, choiceIn.LoginUrl);
-						return await externalDialog.ShowDialogAsync() is DialogResult.OK
-							? ChoiceOut.External(externalDialog.ResponseUrl)
-							: null;
-					}
-				default:
-					throw new Exception($"Unknown {nameof(LoginMethod)} value");
-			}
+			var externalDialog = new LoginExternalDialog(_account, choiceIn.LoginUrl);
+			return await externalDialog.ShowDialogAsync() is DialogResult.OK
+				? ChoiceOut.External(externalDialog.ResponseUrl)
+				: null;
 		}
 
+		private async Task<ChoiceOut?> BrowserLoginAsync(string url)
+		{
+			TaskCompletionSource<ChoiceOut?> tcs = new();
+
+			NativeWebDialog dialog = new()
+			{
+				Title = "Audible Login",
+				CanUserResize = true,
+				Source = new Uri(url)
+			};
+
+			dialog.Closing += (_, _) => tcs.TrySetResult(null);
+			dialog.AdapterCreated += (_, _) =>
+			{
+				if (dialog.TryGetWindow() is Window window)
+				{
+					window.Width = 450;
+					window.Height = 700;
+				}
+			};
+			dialog.NavigationCompleted += async (s, e) =>
+			{
+				if (!e.IsSuccess)
+					return;
+				if (e.Request?.AbsolutePath.StartsWith("/ap/maplanding") is true)
+				{
+					tcs.TrySetResult(ChoiceOut.External(e.Request.ToString()));
+					dialog.Close();
+				}
+				else
+				{
+					await dialog.InvokeScript(getScript(_account.AccountId));
+				}
+			};
+
+			if (!Configuration.IsLinux && App.MainWindow is TopLevel topLevel)
+				dialog.Show(topLevel);
+			else
+				dialog.Show();
+
+			return await tcs.Task;
+		}
+
+		private void Dialog_AdapterCreated(object? sender, WebViewAdapterEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static string getScript(string accountID) => $$"""
+			(function() {
+				function populateForm(){
+					var email = document.querySelector("input[name='email']");
+					if (email !== null)
+						email.value = '{{accountID}}';
+					
+					var pass = document.querySelector("input[name='password']");
+					if (pass !== null)
+						pass.focus();
+				}
+				window.addEventListener("load", (event) => { populateForm(); });
+				populateForm();
+			})()
+			""";
 	}
 }
